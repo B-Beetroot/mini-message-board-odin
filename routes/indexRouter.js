6602929b89b5fca123c8bbe7dabf44c0815b0833
B-Beetroot <216270728+B-Beetroot@users.noreply.github.com>
2026-07-13 06:43:45 +0200

    done assignment

---
+++ routes/indexRouter.js
@@ -0,0 +1,52 @@
+const express = require("express");
+
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+router.get("/", (req, res) => {
+  res.render("index", {
+    title: "Mini Message Board",
+    messages,
+  });
+});
+
+router.get("/new", (req, res) => {
+  res.render("form");
+});
+
+router.post("/new", (req, res) => {
+  const { messageUser, messageText } = req.body;
+
+  messages.push({
+    text: messageText,
+    user: messageUser,
+    added: new Date(),
+  });
+
+  res.redirect("/");
+});
+
+router.get("/message/:id", (req, res) => {
+  const id = req.params.id;
+  const message = messages[id];
+
+  if (!message) {
+    return res.status(404).send("Message not found");
+  }
+
+  res.render("message", { message });
+});
+
+module.exports = router;
